b91e76d2f19c6bd06068ee50ef8c64fa979cd9c6
Arash Shiri <arash.shiri@gmail.com>
2026-02-10 17:05:23 +0100

    Chore: Update copyright year to 2026

--- Generate_block_recorder.py
+++ Generate_block_recorder.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) Arash Shiri 2024 - 2025
+Copyright (C) Arash Shiri 2024 - 2026
 This work is licensed under Creative Commons Attribution-ShareAlike 4.0 International
 https://creativecommons.org/licenses/by-sa/4.0/
 
